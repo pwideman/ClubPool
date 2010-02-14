4a7e6a47c11709d27b84ff43246c054f041bf50a
Paul Wideman <paul.wideman@gmail.com>
2010-02-14 16:50:30 -0600

    Adding ValidatableViewModel and ValidatableExtensions classes

---
+++ src/app/ClubPool.Framework/Extensions/ValidatableExtensions.cs
@@ -0,0 +1,64 @@
+﻿// this code is based on the ValidatableExtensions class in Who Can Help Me (http://whocanhelpme.codeplex.com)
+
+using System.Collections.Generic;
+using System.Linq;
+
+using SharpArch.Core.CommonValidator;
+using SharpArch.Core.NHibernateValidator.CommonValidatorAdapter;
+
+using xVal.ServerSide;
+
+namespace ClubPool.Framework.Extensions
+{
+  /// <summary>
+  /// Extension methods for Entities
+  /// </summary>
+  public static class ValidatableExtensions
+  {
+    /// <summary>
+    /// Validates an object that implements IValidatable and throws a rules exception if there are validation errors
+    /// </summary>
+    /// <param name="validatable">
+    /// The object to validate.
+    /// </param>
+    /// <exception cref="RulesException">
+    /// </exception>
+    public static void Validate(this IValidatable validatable) {
+      if (!validatable.IsValid()) {
+        var errors = new List<ErrorInfo>();
+        var results = validatable.ValidationResults();
+        foreach (var result in results) {
+          errors.Add(result.GetErrorInfo());
+        }
+        throw new RulesException(errors);
+      }
+    }
+
+    /// <summary>
+    /// Gets the ErrorInfo from the validation result and the parent entity type
+    /// </summary>
+    /// <param name="result">
+    /// The validation result.
+    /// </param>
+    /// <returns>
+    /// The ErrorInfo
+    /// </returns>
+    private static ErrorInfo GetErrorInfo(this IValidationResult result) {
+      return GetPropertyLevelErrorInfo(result);
+    }
+
+    /// <summary>
+    /// Returns an ErrorInfo for a property level validation result
+    /// </summary>
+    /// <param name="result">
+    /// The validation result.
+    /// </param>
+    /// <returns>
+    /// The ErrorInfo
+    /// </returns>
+    private static ErrorInfo GetPropertyLevelErrorInfo(IValidationResult result) {
+      return new ErrorInfo(((ValidationResult)result).InvalidValue.PropertyPath, result.Message);
+    }
+
+  }
+}
---
+++ src/app/ClubPool.Web.Controllers/ValidatableViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SharpArch.Core.CommonValidator;
+using Microsoft.Practices.ServiceLocation;
+
+namespace ClubPool.Web.Controllers
+{
+  // this class is heavily influenced by (and most of the code copied from) the ValidatableValueObject
+  // in Who Can Help Me (http://whocanhelpme.codeplex.com/)
+  public class ValidatableViewModel : BaseViewModel, IValidatable
+  {
+    protected IValidator Validator {
+      get { return ServiceLocator.Current.GetInstance<IValidator>(); }
+    }
+
+    public virtual bool IsValid() {
+      return Validator.IsValid(this);
+    }
+
+    public virtual ICollection<IValidationResult> ValidationResults() {
+      return Validator.ValidationResultsFor(this);
+    }
+  }
+}
