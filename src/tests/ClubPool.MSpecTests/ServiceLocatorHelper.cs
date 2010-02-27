1a29c684304fb5d555e02a5c4d458cb5d8ad4166
Paul Wideman <paul.wideman@gmail.com>
2010-02-27 11:26:07 -0600

    Added ServiceLocatorHelper to specs

---
+++ src/tests/ClubPool.MSpecTests/ServiceLocatorHelper.cs
@@ -0,0 +1,45 @@
+﻿// this class is based on the ServiceLocatorHelper in WhoCanHelpMe (http://whocanhelpme.codeplex.com)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.ServiceLocation;
+using Rhino.Mocks;
+using SharpArch.Core.CommonValidator;
+using SharpArch.Core.NHibernateValidator.CommonValidatorAdapter;
+
+namespace ClubPool.MSpecTests
+{
+  public static class ServiceLocatorHelper
+  {
+    private static IServiceLocator provider;
+
+    public static void InitialiseServiceLocator() {
+      provider = MockRepository.GenerateStub<IServiceLocator>();
+
+      ServiceLocator.SetLocatorProvider(() => provider);
+    }
+
+    public static IValidator AddValidator() {
+      if (provider == null) {
+        InitialiseServiceLocator();
+      }
+
+      var validator = new Validator();
+      provider.Stub(p => p.GetInstance<IValidator>()).Return(validator);
+
+      return validator;
+    }
+
+    public static T AddToServiceLocator<T>(this T o) {
+      if (provider == null) {
+        InitialiseServiceLocator();
+      }
+
+      provider.Stub(p => p.GetInstance<T>()).Return(o);
+      return o;
+    }
+  }
+}
