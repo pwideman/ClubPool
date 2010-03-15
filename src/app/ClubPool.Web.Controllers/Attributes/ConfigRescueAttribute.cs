a858ff7fe0947445a7b38741c0a50f1917f31d3b
Paul Wideman <paul.wideman@gmail.com>
2010-03-14 20:03:02 -0500

    Adding ConfigRescueAttribute to control the use of rescues through a config app setting, derive ElmahRescueAttribute from ConfigRescueAttribute

---
+++ src/app/ClubPool.Web.Controllers/Attributes/ConfigRescueAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+using MvcContrib.Filters;
+
+namespace ClubPool.Web.Controllers.Attributes
+{
+  public class ConfigRescueAttribute : RescueAttribute
+  {
+    public ConfigRescueAttribute(string view) : base(view) { }
+    public ConfigRescueAttribute(string view, params Type[] exceptionTypes) : base(view, exceptionTypes) { }
+
+    public override void OnException(ExceptionContext filterContext) {
+      var useRescuesAppSetting = ConfigurationManager.AppSettings["UseRescues"];
+      bool useRescues;
+      if (string.IsNullOrEmpty(useRescuesAppSetting)) {
+        useRescues = true;
+      }
+      else {
+        var parsed = bool.TryParse(useRescuesAppSetting, out useRescues);
+        if (!parsed) {
+          useRescues = true;
+        }
+      }
+      if (useRescues) {
+        base.OnException(filterContext);
+      }
+    }
+  }
+}
--- src/app/ClubPool.Web.Controllers/Attributes/ElmahRescueAttribute.cs
+++ src/app/ClubPool.Web.Controllers/Attributes/ElmahRescueAttribute.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Configuration;
 
 using MvcContrib.Filters;
 using Elmah;
 
 namespace ClubPool.Web.Controllers.Attributes
 {
-  public class ElmahRescueAttribute : RescueAttribute
+  public class ElmahRescueAttribute : ConfigRescueAttribute
   {
     public ElmahRescueAttribute(string view) : base(view) { }
     public ElmahRescueAttribute(string view, params Type[] exceptionTypes) : base(view, exceptionTypes) { }
