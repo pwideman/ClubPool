447cd8722d90334f7dc3f5cac9133f2736b0d7ba
Paul Wideman <paul.wideman@gmail.com>
2010-05-14 17:26:23 -0500

    Added RoleMap

---
+++ src/app/ClubPool.Data/NHibernateMaps/RoleMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+using ClubPool.Core;
+
+namespace ClubPool.Data.NHibernateMaps
+{
+  public class RoleMap : IAutoMappingOverride<Role>
+  {
+    public void Override(AutoMapping<Role> mapping) {
+      mapping.HasManyToMany<User>(x => x.Users)
+        .AsBag()
+        .Table("UsersRoles");
+    }
+  }
+}
