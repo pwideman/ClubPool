50730765b6b1ae9e861e459eb84789339e172755
Paul Wideman <paul.wideman@gmail.com>
2010-05-14 17:27:13 -0500

    Added BuildUrlFromExpression method to BaseController

--- src/app/ClubPool.Web.Controllers/BaseController.cs
+++ src/app/ClubPool.Web.Controllers/BaseController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Web.Mvc;
+using Microsoft.Web.Mvc.Internal;
 
 using ClubPool.Web.Controllers.Attributes;
 
@@ -11,5 +14,26 @@ namespace ClubPool.Web.Controllers
   [ElmahRescue("DefaultError")]
   public abstract class BaseController : Controller
   {
+    protected string BuildUrlFromExpression<T>(Expression<Action<T>> action, RouteValueDictionary values) where T : Controller {
+      var rvd = ExpressionHelper.GetRouteValuesFromExpression(action);
+      foreach (var entry in RouteData.Values) {
+        if (!rvd.ContainsKey(entry.Key)) {
+          rvd.Add(entry.Key, entry.Value);
+        }
+      }
+      if (null != values && values.Count > 0) {
+        foreach (var value in values) {
+          if (!rvd.ContainsKey(value.Key)) {
+            rvd.Add(value.Key, value.Value);
+          }
+        }
+      }
+      if (!rvd.ContainsKey("area")) {
+        rvd.Add("area", "Root");
+      }
+
+      VirtualPathData vpd = ControllerContext.RouteData.Route.GetVirtualPath(ControllerContext.RequestContext, rvd);
+      return (vpd == null) ? null : vpd.VirtualPath;
+    }
   }
 }
