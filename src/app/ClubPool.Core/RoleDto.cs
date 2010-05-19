4ed2ac0ed80ec5c833d4d9d683e759e6765e8860
Paul Wideman <paul.wideman@gmail.com>
2010-05-18 23:28:44 -0500

    Adding User & Role DTOs

---
+++ src/app/ClubPool.Core/RoleDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using NHibernate.Validator.Constraints;
+
+namespace ClubPool.Core
+{
+  public class RoleDto
+  {
+    public int Id { get; set; }
+
+    [DisplayName("Name:")]
+    [NotNullNotEmpty(Message = "Required")]
+    public string Name { get; set; }
+
+    public RoleDto(Role role) {
+      if (null != role) {
+        Id = role.Id;
+        Name = role.Name;
+      }
+    }
+  }
+}
---
+++ src/app/ClubPool.Core/UserDto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using NHibernate.Validator.Constraints;
+
+namespace ClubPool.Core
+{
+  public class UserDto
+  {
+    public int Id { get; set; }
+
+    [DisplayName("Username:")]
+    [NotNullNotEmpty(Message = "Required")]
+    public string Username { get; set; }
+
+    [DisplayName("First")]
+    [NotNullNotEmpty(Message = "Required")]
+    public string FirstName { get; set; }
+
+    [DisplayName("Last")]
+    [NotNullNotEmpty(Message = "Required")]
+    public string LastName { get; set; }
+
+    public string FullName { get; protected set; }
+
+    [DisplayName("Email address:")]
+    [NotNullNotEmpty(Message = "Required")]
+    [Email(Message="Enter a valid email address")]
+    public string Email { get; set; }
+
+    [DisplayName("Approved")]
+    public bool IsApproved { get; set; }
+
+    public string[] Roles { get; set; }
+
+    public UserDto(User user) {
+      if (null != user) {
+        Id = user.Id;
+        Username = user.Username;
+        FirstName = user.FirstName;
+        LastName = user.LastName;
+        FullName = user.FullName;
+        Email = user.Email;
+        IsApproved = user.IsApproved;
+        Roles = user.Roles.Select(r => r.Name).ToArray();
+      }
+    }
+  }
+}
