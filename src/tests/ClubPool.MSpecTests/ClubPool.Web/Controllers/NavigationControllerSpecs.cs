bcddd76e1840fade3c12c3e639126f7e719ec8ad
Paul Wideman <paul.wideman@gmail.com>
2010-08-08 16:46:32 -0500

    Refactored NavigationController specs to use action helpers

--- src/tests/ClubPool.MSpecTests/ClubPool.Web/Controllers/NavigationControllerSpecs.cs
+++ src/tests/ClubPool.MSpecTests/ClubPool.Web/Controllers/NavigationControllerSpecs.cs
@@ -33,54 +33,44 @@ namespace ClubPool.MSpecTests.ClubPool.Web.Controllers
   [Subject(typeof(NavigationController))]
   public class when_asked_for_the_menu_when_user_is_not_logged_in : specification_for_navigation_controller
   {
-    static ActionResult result;
+    static PartialViewResultHelper<MenuViewModel> resultHelper;
 
     Establish context = () => {
       authService.MockPrincipal.MockIdentity.IsAuthenticated = false;
     };
 
-    Because of = () => result = controller.Menu();
+    Because of = () => resultHelper = new PartialViewResultHelper<MenuViewModel>(controller.Menu());
 
-    It should_return_the_default_view = () => result.IsAPartialViewAnd().ViewName.ShouldBeEmpty();
+    It should_indicate_that_the_user_is_not_logged_in = () =>
+      resultHelper.Model.UserIsLoggedIn.ShouldBeFalse();
 
-    It should_set_the_view_model_property_to_a_new_menu_view_model = () =>
-      result.IsAPartialViewAnd().ViewData.Model.ShouldBeOfType(typeof(MenuViewModel));
-
-    It should_set_the_properties_of_the_view_model_correctly = () => {
-      MenuViewModel viewModel = result.IsAPartialViewAnd().ViewData.Model as MenuViewModel;
-      viewModel.DisplayAdminMenu.ShouldBeFalse();
-      viewModel.UserIsLoggedIn.ShouldBeFalse();
-    };
+    It should_indicate_that_the_admin_menu_should_not_be_displayed = () =>
+      resultHelper.Model.DisplayAdminMenu.ShouldBeFalse();
   }
 
   [Subject(typeof(NavigationController))]
   public class when_asked_for_the_menu_when_normal_user_is_logged_in : specification_for_navigation_controller
   {
-    static ActionResult result;
+    static PartialViewResultHelper<MenuViewModel> resultHelper;
 
     Establish context = () => {
       var principal = authService.MockPrincipal;
       principal.MockIdentity.IsAuthenticated = true;
     };
 
-    Because of = () => result = controller.Menu();
+    Because of = () => resultHelper = new PartialViewResultHelper<MenuViewModel>(controller.Menu());
 
-    It should_return_the_default_view = () => result.IsAPartialViewAnd().ViewName.ShouldBeEmpty();
+    It should_indicate_that_the_user_is_logged_in = () =>
+      resultHelper.Model.UserIsLoggedIn.ShouldBeTrue();
 
-    It should_set_the_view_model_property_to_a_new_menu_view_model = () =>
-      result.IsAPartialViewAnd().ViewData.Model.ShouldBeOfType(typeof(MenuViewModel));
-
-    It should_set_the_properties_of_the_view_model_correctly = () => {
-      MenuViewModel viewModel = result.IsAPartialViewAnd().ViewData.Model as MenuViewModel;
-      viewModel.DisplayAdminMenu.ShouldBeFalse();
-      viewModel.UserIsLoggedIn.ShouldBeTrue();
-    };
+    It should_indicate_that_the_admin_menu_should_not_be_displayed = () =>
+      resultHelper.Model.DisplayAdminMenu.ShouldBeFalse();
   }
 
   [Subject(typeof(NavigationController))]
   public class when_asked_for_the_menu_when_admin_user_is_logged_in : specification_for_navigation_controller
   {
-    static ActionResult result;
+    static PartialViewResultHelper<MenuViewModel> resultHelper;
 
     Establish context = () => {
       var principal = authService.MockPrincipal;
@@ -88,17 +78,12 @@ namespace ClubPool.MSpecTests.ClubPool.Web.Controllers
       principal.Roles = new string[] { Roles.Administrators };
     };
 
-    Because of = () => result = controller.Menu();
+    Because of = () => resultHelper = new PartialViewResultHelper<MenuViewModel>(controller.Menu());
 
-    It should_return_the_default_view = () => result.IsAPartialViewAnd().ViewName.ShouldBeEmpty();
+    It should_indicate_that_the_user_is_logged_in = () =>
+      resultHelper.Model.UserIsLoggedIn.ShouldBeTrue();
 
-    It should_set_the_view_model_property_to_a_new_menu_view_model = () =>
-      result.IsAPartialViewAnd().ViewData.Model.ShouldBeOfType(typeof(MenuViewModel));
-
-    It should_set_the_properties_of_the_view_model_correctly = () => {
-      MenuViewModel viewModel = result.IsAPartialViewAnd().ViewData.Model as MenuViewModel;
-      viewModel.DisplayAdminMenu.ShouldBeTrue();
-      viewModel.UserIsLoggedIn.ShouldBeTrue();
-    };
+    It should_indicate_that_the_admin_menu_should_be_displayed = () =>
+      resultHelper.Model.DisplayAdminMenu.ShouldBeTrue();
   }
 }
