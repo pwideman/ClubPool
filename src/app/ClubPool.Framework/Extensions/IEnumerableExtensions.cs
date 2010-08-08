f0f7f5e6424545ee0988417ed7de919a5c041e9d
Paul Wideman <paul.wideman@gmail.com>
2010-08-08 16:22:38 -0500

    Added IEnumerableExtensions

---
+++ src/app/ClubPool.Framework/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SharpArch.Core;
+
+namespace ClubPool.Framework.Extensions
+{
+  public static class IEnumerableExtensions
+  {
+    public static void Each<T>(this IEnumerable<T> items, Action<T> action) {
+      Check.Require(null != items, "items cannot be null");
+      Check.Require(null != action, "action cannot be null");
+
+      foreach (T item in items) {
+        action(item);
+      }
+    }
+  }
+}
