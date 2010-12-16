cfb39f8959a6c398a82dda7488b49871bddbcc99
Paul Wideman <paul.wideman@gmail.com>
2010-12-15 18:53:55 -0600

    Working on currentseason/schedule

---
+++ src/app/ClubPool.Web.Controllers/CurrentSeason/ViewModels/CurrentSeasonScheduleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClubPool.Web.Controllers.CurrentSeason.ViewModels
+{
+  public class CurrentSeasonScheduleViewModel
+  {
+  }
+
+  public class DivisionViewModel
+  {
+  }
+}
