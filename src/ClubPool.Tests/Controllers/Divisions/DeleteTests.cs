bb5af8325e31824f750cd5d4f78c22414e66062b
pwideman <paul.wideman@gmail.com>
2011-12-08 13:47:03 -0600

    Ported divisions/delete and divisions/edit (get) tests

---
+++ src/ClubPool.Tests/Controllers/Divisions/DeleteTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Moq;
+using FluentAssertions;
+using NUnit.Framework;
+
+using ClubPool.Testing;
+using ClubPool.Web.Models;
+using ClubPool.Web.Controllers;
+
+namespace ClubPool.Tests.Controllers.Divisions.when_asked_to_delete_a_division
+{
+  [TestFixture]
+  public class that_does_not_exist : DivisionsControllerTest
+  {
+    private HttpNotFoundResultHelper resultHelper;
+
+    public override void When() {
+      resultHelper = new HttpNotFoundResultHelper(controller.Delete(0));
+    }
+
+    [Test]
+    public void it_should_return_an_http_not_found_result() {
+      resultHelper.Result.Should().NotBeNull();
+    }
+  }
+
+  [TestFixture]
+  public class with_no_teams : DivisionsControllerTest
+  {
+    private RedirectToRouteResultHelper resultHelper;
+    private Division division;
+
+    public override void Given() {
+      var season = new Season("temp", GameType.EightBall);
+      season.SetIdTo(1);
+      division = new Division("temp", DateTime.Now, season);
+      season.AddDivision(division);
+      division.SetIdTo(1);
+      repository.Setup(r => r.Get<Division>(division.Id)).Returns(division);
+    }
+
+    public override void When() {
+      resultHelper = new RedirectToRouteResultHelper(controller.Delete(division.Id));
+    }
+
+    [Test]
+    public void it_should_delete_the_division() {
+      repository.Verify(r => r.Delete(division), Times.Once());
+    }
+
+    [Test]
+    public void it_should_return_a_notification_message() {
+      controller.TempData.ContainsKey(GlobalViewDataProperty.PageNotificationMessage).Should().BeTrue();
+    }
+
+    [Test]
+    public void it_should_redirect_to_the_view_season_view() {
+      resultHelper.ShouldRedirectTo("seasons", "view");
+    }
+  }
+
+}
---
+++ src/ClubPool.Tests/Controllers/Divisions/EditTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+
+using ClubPool.Testing;
+using ClubPool.Web.Controllers.Divisions.ViewModels;
+using ClubPool.Web.Models;
+
+namespace ClubPool.Tests.Controllers.Divisions.when_asked_for_the_edit_view
+{
+  [TestFixture]
+  public class for_an_invalid_division : DivisionsControllerTest
+  {
+    private HttpNotFoundResultHelper resultHelper;
+
+    public override void When() {
+      resultHelper = new HttpNotFoundResultHelper(controller.Edit(0));
+    }
+
+    [Test]
+    public void it_should_return_an_http_not_found_result() {
+      resultHelper.Result.Should().NotBeNull();
+    }
+  }
+
+  [TestFixture]
+  public class for_a_valid_division : DivisionsControllerTest
+  {
+    private ViewResultHelper<EditDivisionViewModel> resultHelper;
+    private Division division;
+
+    public override void Given() {
+      division = new Division("temp", DateTime.Now, new Season("temp", GameType.EightBall));
+      division.SetIdTo(1);
+      repository.Setup(r => r.Get<Division>(division.Id)).Returns(division);
+    }
+
+    public override void When() {
+      resultHelper = new ViewResultHelper<EditDivisionViewModel>(controller.Edit(division.Id));
+    }
+
+    [Test]
+    public void it_should_initialize_the_id_field() {
+      resultHelper.Model.Id.Should().Be(division.Id);
+    }
+
+    [Test]
+    public void it_should_initialize_the_name_field() {
+      resultHelper.Model.Name.Should().Be(division.Name);
+    }
+
+    [Test]
+    public void it_should_initialize_the_starting_date_field() {
+      resultHelper.Model.StartingDate.Should().Be(division.StartingDate.ToShortDateString());
+    }
+
+    [Test]
+    public void it_should_initialize_the_season_name_field() {
+      resultHelper.Model.SeasonName.Should().Be(division.Season.Name);
+    }
+  }
+}
