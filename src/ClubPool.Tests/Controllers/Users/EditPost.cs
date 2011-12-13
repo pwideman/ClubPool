99bd4143a83bae2504a6743f7e84bbcfa1f0364e
Paul Wideman <paul.wideman@gmail.com>
2011-12-13 11:30:22 -0600

    Started porting users/edit (HTTPPOST) tests

---
+++ src/ClubPool.Tests/Controllers/Users/EditPost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Moq;
+using NUnit.Framework;
+using FluentAssertions;
+
+using ClubPool.Web.Controllers.Users.ViewModels;
+using ClubPool.Testing;
+
+namespace ClubPool.Tests.Controllers.Users
+{
+  public abstract class EditPostTest : EditTest
+  {
+    protected EditViewModel viewModel;
+    protected bool isApproved = true;
+    protected bool isLocked = false;
+
+    public override void EstablishContext() {
+      base.EstablishContext();
+      viewModel = new EditViewModel() {
+        FirstName = "first",
+        LastName = "last",
+        Username = "newusername",
+        Email = "newemail@email.com",
+        IsLocked = !isLocked,
+        IsApproved = !isApproved,
+        Id = user.Id,
+        Roles = new int[] { adminRole.Id, officerRole.Id },
+        Version = DomainModelHelper.ConvertIntVersionToString(1),
+        Password = "newpass",
+        ConfirmPassword = "newpass"
+      };
+
+      user.IsLocked = isLocked;
+      user.IsApproved = isApproved;
+
+      membershipService.Setup(s => s.EncodePassword(It.IsAny<string>(), It.IsAny<string>()))
+        .Returns<string, string>((password, salt) => password);
+    }
+  }
+}
