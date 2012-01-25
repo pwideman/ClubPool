edc197202b9d44b0dae0a5f9fd14387fca22cb5f
Paul Wideman <paul.wideman@gmail.com>
2012-01-24 19:26:01 -0600

    Moved /scoresheet view model init logic out of view models and into controller

--- src/ClubPool.Web/Controllers/Scoresheet/ScoresheetController.cs
+++ src/ClubPool.Web/Controllers/Scoresheet/ScoresheetController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Linq;
+using System.Collections.Generic;
 
 using ClubPool.Web.Models;
 using ClubPool.Web.Infrastructure;
@@ -18,8 +20,85 @@ namespace ClubPool.Web.Controllers.Scoresheet
     [Authorize]
     public ActionResult Index(int id) {
       var meet = repository.Get<Meet>(id);
-      var viewModel = new ScoresheetViewModel(meet);
+      var viewModel = CreateScoresheetViewModel(meet);
       return View(viewModel);
     }
+
+    private ScoresheetViewModel CreateScoresheetViewModel(Meet meet) {
+      var model = new ScoresheetViewModel();
+      model.Id = meet.Id;
+      model.ScheduledWeek = meet.Week + 1;
+      model.ScheduledDate = meet.Division.StartingDate.AddDays(meet.Week * 7);
+      var teams = meet.Teams.ToArray();
+      var team1 = teams[0];
+      var team2 = teams[1];
+      model.Team1Name = team1.Name;
+      model.Team2Name = team2.Name;
+      var matches = new List<ScoresheetMatchViewModel>();
+      foreach (var match in meet.Matches) {
+        matches.Add(CreateScoresheetMatchViewModel(match));
+      }
+      model.Matches = matches;
+      return model;
+    }
+
+    private ScoresheetMatchViewModel CreateScoresheetMatchViewModel(Match match) {
+      var model = new ScoresheetMatchViewModel();
+      model.Id = match.Id;
+      var gameType = match.Meet.Division.Season.GameType;
+      var players = match.Players.ToArray();
+      model.Player1 = CreatePlayerViewModel(players[0].Player, gameType);
+      model.Player2 = CreatePlayerViewModel(players[1].Player, gameType);
+
+      model.Player1.GamesToWin = CalculateGamesToWin(model.Player1.SkillLevel, model.Player2.SkillLevel);
+      model.Player2.GamesToWin = CalculateGamesToWin(model.Player2.SkillLevel, model.Player1.SkillLevel);
+      return model;
+    }
+
+    private int CalculateGamesToWin(int skillLevel, int opponentSkillLevel) {
+      int gtw = 0;
+      int maxDifference = 1; // number of games to reduce skill level by
+      // compute GTW
+      if (0 == skillLevel || 0 == opponentSkillLevel) {
+        gtw = 4;
+      }
+      else {
+        int difference = 0;
+        if (skillLevel > opponentSkillLevel) {
+          if (opponentSkillLevel > 3) {
+            difference = opponentSkillLevel - 3;
+            if (difference > maxDifference) {
+              difference = maxDifference;
+            }
+          }
+        }
+        else {
+          if (skillLevel > 3) {
+            difference = skillLevel - 3;
+          }
+          if (difference > maxDifference) {
+            difference = maxDifference;
+          }
+        }
+        gtw = skillLevel - difference;
+      }
+      return gtw;
+    }
+
+    private PlayerViewModel CreatePlayerViewModel(User player, GameType gameType) {
+      var model = new PlayerViewModel();
+      model.Id = player.Id;
+      model.Name = player.FullName;
+
+      var slQuery = player.SkillLevels.Where(sl => sl.GameType == gameType);
+      if (slQuery.Any()) {
+        model.SkillLevel = slQuery.First().Value;
+      }
+      else {
+        model.SkillLevel = 0;
+      }
+      return model;
+    }
+
   }
 }
--- src/ClubPool.Web/Controllers/Scoresheet/ViewModels.cs
+++ src/ClubPool.Web/Controllers/Scoresheet/ViewModels.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-
-using ClubPool.Web.Models;
 
 namespace ClubPool.Web.Controllers.Scoresheet
 {
@@ -13,23 +10,7 @@ namespace ClubPool.Web.Controllers.Scoresheet
     public DateTime ScheduledDate { get; set; }
     public string Team1Name { get; set; }
     public string Team2Name { get; set; }
-    public IEnumerable<ScoresheetMatchViewModel> Matches { get; protected set; }
-
-    public ScoresheetViewModel(Meet meet) {
-      Id = meet.Id;
-      ScheduledWeek = meet.Week + 1;
-      ScheduledDate = meet.Division.StartingDate.AddDays(meet.Week * 7);
-      var teams = meet.Teams.ToArray();
-      var team1 = teams[0];
-      var team2 = teams[1];
-      Team1Name = team1.Name;
-      Team2Name = team2.Name;
-      var matches = new List<ScoresheetMatchViewModel>();
-      foreach (var match in meet.Matches) {
-        matches.Add(new ScoresheetMatchViewModel(match));
-      }
-      Matches = matches;
-    }
+    public IEnumerable<ScoresheetMatchViewModel> Matches { get; set; }
   }
 
   public class ScoresheetMatchViewModel
@@ -37,48 +18,6 @@ namespace ClubPool.Web.Controllers.Scoresheet
     public PlayerViewModel Player1;
     public PlayerViewModel Player2;
     public int Id;
-
-    public ScoresheetMatchViewModel(Match match) {
-      Id = match.Id;
-
-      var gameType = match.Meet.Division.Season.GameType;
-      var players = match.Players.ToArray();
-      Player1 = new PlayerViewModel(players[0].Player, gameType);
-      Player2 = new PlayerViewModel(players[1].Player, gameType);
-
-      Player1.GamesToWin = CalculateGamesToWin(Player1.SkillLevel, Player2.SkillLevel);
-      Player2.GamesToWin = CalculateGamesToWin(Player2.SkillLevel, Player1.SkillLevel);
-    }
-
-    private int CalculateGamesToWin(int skillLevel, int opponentSkillLevel) {
-      int gtw = 0;
-      int maxDifference = 1; // number of games to reduce skill level by
-      // compute GTW
-      if (0 == skillLevel || 0 == opponentSkillLevel) {
-        gtw = 4;
-      }
-      else {
-        int difference = 0;
-        if (skillLevel > opponentSkillLevel) {
-          if (opponentSkillLevel > 3) {
-            difference = opponentSkillLevel - 3;
-            if (difference > maxDifference) {
-              difference = maxDifference;
-            }
-          }
-        }
-        else {
-          if (skillLevel > 3) {
-            difference = skillLevel - 3;
-          }
-          if (difference > maxDifference) {
-            difference = maxDifference;
-          }
-        }
-        gtw = skillLevel - difference;
-      }
-      return gtw;
-    }
   }
 
   public class PlayerViewModel
@@ -87,18 +26,5 @@ namespace ClubPool.Web.Controllers.Scoresheet
     public string Name { get; set; }
     public int SkillLevel { get; set; }
     public int GamesToWin { get; set; }
-
-    public PlayerViewModel(User player, GameType gameType) {
-      Id = player.Id;
-      Name = player.FullName;
-
-      var slQuery = player.SkillLevels.Where(sl => sl.GameType == gameType);
-      if (slQuery.Any()) {
-        SkillLevel = slQuery.First().Value;
-      }
-      else {
-        SkillLevel = 0;
-      }
-    }
   }
 }
