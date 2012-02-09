54e6c35ae258999e4d8fd7c451f221cd2f72fd63
Paul Wideman <paul.wideman@gmail.com>
2012-02-08 21:20:29 -0600

    Converted /teams/edit and /teams/create to use new view script registration mechanism

---
+++ src/ClubPool.Web/Scripts/teams.js
@@ -0,0 +1,6 @@
+﻿(function (scriptRegistrar) {
+  scriptRegistrar.registerViewScript("teams/_editteam", function () {
+    $("#Name").focus();
+    $(".integer-input").numeric();
+  });
+})($scriptRegistrar);
