2e7ba14cdb6bd8c4df1a157fe8d5560837364040
Paul Wideman <paul.wideman@gmail.com>
2012-02-08 21:17:09 -0600

    Added view script registration mechanism

---
+++ src/ClubPool.Web/Infrastructure/ScriptExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Text;
+
+namespace ClubPool.Web.Infrastructure
+{
+  public static class ScriptExtensions
+  {
+    private static List<string> registrations = new List<string>();
+
+    public static void RegisterScriptView(this HtmlHelper helper, string viewName) {
+      if (!registrations.Contains(viewName)) {
+        registrations.Add(viewName);
+      }
+    }
+
+    public static MvcHtmlString RenderRegisteredScriptViews(this HtmlHelper helper) {
+      if (registrations.Any()) {
+        var tagFormat = @"<script type=""text/javascript"">$(function(){{{0}}})</script>";
+        var registrationFormat = @"$scriptRegistrar.initViewScript(""{0}"");";
+        var scripts = new StringBuilder();
+        foreach (var viewName in registrations) {
+          scripts.Append(string.Format(registrationFormat, viewName));
+        }
+        return MvcHtmlString.Create(string.Format(tagFormat, scripts.ToString()));
+      }
+      else {
+        return MvcHtmlString.Empty;
+      }
+    }
+  }
+}
---
+++ src/ClubPool.Web/Scripts/scriptRegistrar.js
@@ -0,0 +1,22 @@
+﻿var $scriptRegistrar = (function () {
+  var my = {};
+  var registrations = {};
+
+  my.registerViewScript = function (name, fn) {
+    if (!registrations[name]) {
+      registrations[name] = [];
+    }
+    registrations[name].push(fn);
+  };
+
+  my.initViewScript = function (name) {
+    if (registrations[name]) {
+      var scripts = registrations[name];
+      for (var i=0; i < scripts.length; i++) {
+        scripts[i]();
+      }
+    }
+  }
+
+  return my;
+} ());
