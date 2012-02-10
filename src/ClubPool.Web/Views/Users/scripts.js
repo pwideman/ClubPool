37c51c69d736c0346f5eba760e018dc811da03fb
Paul Wideman <paul.wideman@gmail.com>
2012-02-09 21:58:26 -0600

    Moved /users/details scripts to view script pattern

---
+++ src/ClubPool.Web/Views/Users/scripts.js
@@ -0,0 +1,8 @@
+﻿(function (sr) {
+  sr.registerViewScript("users/details", function () {
+    $(document).ready(function () {
+      $("#tabs").tabs();
+    });
+    $("#skill_level_calc tr:last").addClass("last");
+  });
+})($.scriptRegistrar);
