e74940df5fe605dbe66c6b4c4504134c93241580
Paul Wideman <paul.wideman@gmail.com>
2012-02-09 22:01:28 -0600

    Moved /users/index to view script pattern

--- src/ClubPool.Web/Views/Users/scripts.js
+++ src/ClubPool.Web/Views/Users/scripts.js
@@ -1,8 +1,36 @@
 ﻿(function (sr) {
+  // details
   sr.registerViewScript("users/details", function () {
     $(document).ready(function () {
       $("#tabs").tabs();
     });
     $("#skill_level_calc tr:last").addClass("last");
+  });
+
+  // index
+  sr.registerViewScript("users/index", function () {
+    $("tbody.content tr:odd").addClass("alt");
+    $("#q").placeholder().keypress(function (e) {
+      if (e.keyCode === 13) {
+        doSearch($(this).val());
+      }
+    });
+    $("#users-search-icon").click(function(e) {
+      doSearch($("#q").val());
+    });
+
+    function doSearch(query) {
+      if (query !== $.query.get("q")) {
+        var search = "";
+        if (query) {
+          search = $.query.set("q", query).remove("page");
+        }
+        else {
+          search = $.query.remove("q").remove("page");
+        }
+        var newurl = location.origin + location.pathname + search;
+        location.assign(newurl);
+      }
+    }
   });
 })($.scriptRegistrar);
