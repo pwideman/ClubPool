39f45d4d5555497274e16650e1c1650e2867769f
Paul Wideman <paul.wideman@gmail.com>
2012-02-09 21:19:47 -0600

    Moved seasons/details script to view script pattern

---
+++ src/ClubPool.Web/Views/Seasons/scripts.js
@@ -0,0 +1,15 @@
+﻿(function (sr) {
+  sr.registerViewScript("seasons/details", function () {
+    $("tbody.content tr:odd").addClass("alt");
+    $("#divisiontabs").tabs({
+      cookie: {}
+    });
+    $(".division-details-tabs").tabs({
+      cookie: {}
+    });
+    $(".submit-form-link").click(function () {
+      $(this).parents("form:first").submit();
+    });
+    $(".schedule-byes").numeric();
+  });
+})($.scriptRegistrar);
