9d70b39a16bfd526a7a235ea9077550300043e32
Paul Wideman <paul.wideman@gmail.com>
2012-02-09 20:15:42 -0600

    Updated dashboard script to new view script pattern

---
+++ src/ClubPool.Web/Scripts/dashboard.js
@@ -0,0 +1,8 @@
+﻿(function (sr) {
+  sr.registerViewScript("dashboard", function () {
+    $("#lastmeet tbody tr:odd:not(:last)").addClass("match");
+    $("#lastmeet tbody tr:even").addClass("player");
+    $("#season_results tr:last").addClass("last");
+    $("#skill_level_calc tr:last").addClass("last");
+  });
+})($.scriptRegistrar);
