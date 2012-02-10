cb4a9f8db5ab6894e214921952dd787dbc4f177e
Paul Wideman <paul.wideman@gmail.com>
2012-02-09 21:45:40 -0600

    Moved /unapprovedusers to view script pattern

---
+++ src/ClubPool.Web/Views/UnapprovedUsers/scripts.js
@@ -0,0 +1,9 @@
+﻿(function (sr) {
+  sr.registerViewScript("unapprovedusers", function () {
+    $("#approveall").click(function () {
+      var approved = $(this).attr("checked");
+      $("input[name=userids]").attr("checked", approved);
+    });
+    $("#unapproved_users tbody.content tr:odd").addClass("alt");
+  });
+})($.scriptRegistrar);
