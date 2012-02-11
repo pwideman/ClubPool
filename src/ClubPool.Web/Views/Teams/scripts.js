286005a6c4b1371e460eab202682e929dbd48b4d
Paul Wideman <paul.wideman@gmail.com>
2012-02-10 21:15:47 -0600

    Moved /teams/details script to view script pattern

--- src/ClubPool.Web/Views/Teams/scripts.js
+++ src/ClubPool.Web/Views/Teams/scripts.js
@@ -1,7 +1,57 @@
 ﻿(function (sr) {
-  // teams/_editteam partial
+  // _editteam partial
   sr.registerViewScript("teams/_editteam", function () {
     $("#Name").focus();
     $(".integer-input").numeric();
+  });
+
+  // details
+  sr.registerViewScript("teams/details", function () {
+    $("#season-results-table tbody:last").removeClass("meet").find("tr:last").addClass("last");
+  });
+
+  // details - if user can edit name
+  sr.registerViewScript("teams/details.editname", function () {
+    // declare formOpts explicitly, since we need to use it in two different places
+    var formOpts = {
+      success: function (response, status, xhr, form) {
+        if (xhr.status === 200) {
+          if (response.Success) {
+            $currentTeamName = $("#name").val();
+            $("#name").effect("highlight", 1500);
+          }
+          else {
+            $("#name").ajaxUpdateError({ message: response.Message });
+            $("#name").val($currentTeamName);
+          }
+        }
+      },
+      error: function (xhr, status, error) {
+        $("#name").ajaxUpdateError({ message: "An error occurred on the server while saving your changes, try again" });
+      },
+      beforeSubmit: function () {
+        var valid = $("#update_name_form").validate().form();
+        var different = $("#name").val() != $currentTeamName;
+        var ret = valid && different;
+        $("#name").ajaxUpdateError("close");
+        return ret;
+      }
+    };
+
+    // set up team name text box event handlers
+    $("#name").blur(saveName).focus(function (e) {
+      $currentTeamName = $(this).val();
+    });
+    // create ajax form
+    $("#update_name_form").ajaxForm(formOpts);
+
+    // set up validation
+    $("#update_name_form").validate({
+      errorPlacement: function () { }
+    });
+
+    function saveName() {
+      $("#update_name_form").ajaxSubmit(formOpts);
+    }
   });
 })($.scriptRegistrar);
