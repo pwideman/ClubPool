2a2fbc1d8ec33f34814a2e75cb69665a4a053718
Paul Wideman <paul.wideman@gmail.com>
2012-02-12 00:01:09 -0600

    Moved menu init script to navigation/menu view script

--- src/ClubPool.Web/Scripts/clubpool.js
+++ src/ClubPool.Web/Scripts/clubpool.js
@@ -1,14 +1,5 @@
 ﻿// initialise plugins
 $(function () {
-  // set up the menu
-  $('ul.sf-menu').supersubs({
-    minWidth: 12,
-    maxWidth: 27,
-    extraWidth: 1
-  }).superfish({
-    animation: { opacity: "show" },
-    speed: "fast"
-  });
   $(".notification").effect("fade", { easing: "easeInExpo" }, 5000);
 });
 
---
+++ src/ClubPool.Web/Views/Navigation/scripts.js
@@ -0,0 +1,13 @@
+﻿(function (sr) {
+  sr.registerViewScript("navigation/menu", function () {
+    // set up the menu
+    $('ul.sf-menu').supersubs({
+      minWidth: 12,
+      maxWidth: 27,
+      extraWidth: 1
+    }).superfish({
+      animation: { opacity: "show" },
+      speed: "fast"
+    });
+  });
+})($.scriptRegistrar);
