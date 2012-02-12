21db8ae0ddefc5166a2b2a98b5b0c36c872594e6
Paul Wideman <paul.wideman@gmail.com>
2012-02-12 01:37:50 -0600

    Added RenderSiteScripts HtmlHelper extension method to render scripts in debug or release mode; removed SquishIt related extension methods

--- src/ClubPool.Web/Infrastructure/ScriptExtensions.cs
+++ src/ClubPool.Web/Infrastructure/ScriptExtensions.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using System.Text;
 using System.IO;
-using System.Diagnostics;
-
-using SquishIt.Framework.JavaScript;
 
 namespace ClubPool.Web.Infrastructure
 {
@@ -22,8 +17,8 @@ namespace ClubPool.Web.Infrastructure
     public static MvcHtmlString RenderRegisteredScriptViews(this HtmlHelper helper) {
       var registrar = DependencyResolver.Current.GetService<ScriptViewRegistrar>();
       if (registrar.HasRegisteredScriptViews) {
-        var tagFormat = @"<script type=""text/javascript"">$(function(){{{0}}})</script>";
-        var registrationFormat = @"$.scriptRegistrar.initViewScript(""{0}"");";
+        var tagFormat = "<script type=\"text/javascript\">\n$(function(){{\n{0}\n}});\n</script>";
+        var registrationFormat = "$.scriptRegistrar.initViewScript(\"{0}\");\n";
         var scripts = new StringBuilder();
         foreach (var viewName in registrar.GetScriptViews()) {
           scripts.Append(string.Format(registrationFormat, viewName));
@@ -35,46 +30,40 @@ namespace ClubPool.Web.Infrastructure
       }
     }
 
-    private static IEnumerable<string> viewScripts;
-    private static bool viewsDirectoryScanned = false;
-    private static object syncLock = new object();
 
-    public static JavaScriptBundle AddViewScripts(this JavaScriptBundle bundle, bool forceRelease = false) {
-      if (!forceRelease && HttpContext.Current.IsDebuggingEnabled) {
-        lock (syncLock) {
-          viewScripts = ScanViewsDirectory();
-          viewsDirectoryScanned = true;
-        }
-      }
-      else if (!viewsDirectoryScanned) {
-        lock (syncLock) {
-          if (!viewsDirectoryScanned) {
-            viewScripts = ScanViewsDirectory();
-            viewsDirectoryScanned = true;
-          }
-        }
-      }
-      bundle.Add(viewScripts.ToArray());
-      return bundle;
-    }
-
-    private static IEnumerable<string> ScanViewsDirectory() {
+    public static MvcHtmlString RenderSiteScripts(this HtmlHelper helper, bool forceRelease = false) {
       var appPath = DependencyResolver.Current.GetService<Configuration.ClubPoolConfiguration>().AppRootPath;
-      var scriptFiles = new List<string>();
-      var pathToScan = Path.Combine(appPath, "Views");
-      Trace.TraceInformation("Scanning path for view scripts: " + pathToScan);
-      ScanDirectoryForScripts(pathToScan, scriptFiles);
-      return scriptFiles.Select(s => s.Replace(appPath, "~/"));
+      if (!forceRelease && HttpContext.Current.IsDebuggingEnabled) {
+        var scripts = new List<string>();
+        AddScripts(scripts, Path.Combine(appPath, "scripts\\lib"), "*.js", false);
+        AddScripts(scripts, Path.Combine(appPath, "scripts"), "*.js", true);
+        AddScripts(scripts, Path.Combine(appPath, "views"), "scripts.js", true, true);
+        return RenderScriptTags(scripts);
+      }
+      else {
+        // release
+        return RenderScriptTags(new string[] { Path.Combine(appPath, "Scripts\\site.min.js") });
+      }
     }
 
-    private static void ScanDirectoryForScripts(string directory, List<string> scriptFiles) {
-      var myScriptFiles = Directory.GetFiles(directory, "scripts.js");
+    private static void AddScripts(List<string> scripts, string folder, string searchPattern, bool filterMinified = true, bool recursive = false) {
+      var myScriptFiles = Directory.GetFiles(folder, searchPattern, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
       foreach (var file in myScriptFiles) {
-        scriptFiles.Add(file);
+        if (!filterMinified || !file.EndsWith(".min.js")) {
+          scripts.Add(file);
+        }
       }
-      foreach (var subdirectory in Directory.GetDirectories(directory)) {
-        ScanDirectoryForScripts(subdirectory, scriptFiles);
+    }
+
+    private static MvcHtmlString RenderScriptTags(IEnumerable<string> scriptFiles) {
+      var appPath = DependencyResolver.Current.GetService<Configuration.ClubPoolConfiguration>().AppRootPath;
+      var tagFormat = @"<script type=""text/javascript"" src=""{0}""></script>";
+      var tags = new StringBuilder();
+      foreach (var file in scriptFiles) {
+        var scriptUrl = VirtualPathUtility.ToAbsolute(file.Replace(appPath, "~/"));
+        tags.AppendLine(string.Format(tagFormat, scriptUrl));
       }
+      return MvcHtmlString.Create(tags.ToString());
     }
   }
 }
