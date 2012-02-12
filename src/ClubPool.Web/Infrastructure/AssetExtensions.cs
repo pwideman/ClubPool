323062214024fc043e20d9949550807a7f195536
Paul Wideman <paul.wideman@gmail.com>
2012-02-12 16:21:07 -0600

    Load jQuery UI locally when in debug

--- src/ClubPool.Web/Infrastructure/AssetExtensions.cs
+++ src/ClubPool.Web/Infrastructure/AssetExtensions.cs
@@ -11,17 +11,26 @@ namespace ClubPool.Web.Infrastructure
     private const string LocalAssetBasePath = "~/content/assets";
     private const string RemoteAssetBasePath = "http://ajax.googleapis.com/ajax/libs";
     private const string JQueryPath = "/jquery/1.7.1/jquery.min.js";
+    private const string JQueryUIScriptPath = "/jqueryui/1.8.17/jquery-ui.min.js";
     private const string ScriptTagFormat = "<script type=\"text/javascript\" src=\"{0}\"></script>";
 
-    public static MvcHtmlString JQuery(this HtmlHelper helper, bool forceRelease = false) {
+    private static MvcHtmlString ScriptAsset(string localAsset, string remoteAsset, bool forceRelease, HtmlHelper helper) {
       string scriptPath = null;
       if (!forceRelease && helper.ViewContext.HttpContext.IsDebuggingEnabled) {
-        scriptPath = VirtualPathUtility.ToAbsolute(LocalAssetBasePath + "/jquery.min.js");
+        scriptPath = VirtualPathUtility.ToAbsolute(LocalAssetBasePath + "/" + localAsset);
       }
       else {
-        scriptPath = RemoteAssetBasePath + JQueryPath;
+        scriptPath = RemoteAssetBasePath + "/" + remoteAsset;
       }
       return MvcHtmlString.Create(string.Format(ScriptTagFormat, scriptPath));
+    }
+
+    public static MvcHtmlString JQuery(this HtmlHelper helper, bool forceRelease = false) {
+      return ScriptAsset("jquery.min.js", JQueryPath, forceRelease, helper);
+    }
+
+    public static MvcHtmlString JQueryUIScript(this HtmlHelper helper, bool forceRelease = false) {
+      return ScriptAsset("jquery-ui.min.js", JQueryUIScriptPath, forceRelease, helper);
     }
   }
 }
