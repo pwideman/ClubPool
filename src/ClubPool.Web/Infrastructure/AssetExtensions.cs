5a57ca80af130e8488b4c9d23660ee89a8ede8cb
Paul Wideman <paul.wideman@gmail.com>
2012-02-12 16:31:17 -0600

    Load jquery ui css locally when in debug, had to add jquery-ui.css and all images to /content/assets

--- src/ClubPool.Web/Infrastructure/AssetExtensions.cs
+++ src/ClubPool.Web/Infrastructure/AssetExtensions.cs
@@ -12,17 +12,27 @@ namespace ClubPool.Web.Infrastructure
     private const string RemoteAssetBasePath = "http://ajax.googleapis.com/ajax/libs";
     private const string JQueryPath = "/jquery/1.7.1/jquery.min.js";
     private const string JQueryUIScriptPath = "/jqueryui/1.8.17/jquery-ui.min.js";
+    private const string JQueryUICssPath = "/jqueryui/1.8.17/themes/redmond/jquery-ui.css";
     private const string ScriptTagFormat = "<script type=\"text/javascript\" src=\"{0}\"></script>";
+    private const string CssTagFormat = "<link rel=\"stylesheet\" type=\"text/css\" href=\"{0}\" />";
 
-    private static MvcHtmlString ScriptAsset(string localAsset, string remoteAsset, bool forceRelease, HtmlHelper helper) {
-      string scriptPath = null;
+    private static MvcHtmlString Asset(string tagFormat, string localAsset, string remoteAsset, bool forceRelease, HtmlHelper helper) {
+      string assetPath = null;
       if (!forceRelease && helper.ViewContext.HttpContext.IsDebuggingEnabled) {
-        scriptPath = VirtualPathUtility.ToAbsolute(LocalAssetBasePath + "/" + localAsset);
+        assetPath = VirtualPathUtility.ToAbsolute(LocalAssetBasePath + "/" + localAsset);
       }
       else {
-        scriptPath = RemoteAssetBasePath + "/" + remoteAsset;
+        assetPath = RemoteAssetBasePath + "/" + remoteAsset;
       }
-      return MvcHtmlString.Create(string.Format(ScriptTagFormat, scriptPath));
+      return MvcHtmlString.Create(string.Format(tagFormat, assetPath));
+    }
+
+    private static MvcHtmlString ScriptAsset(string localScript, string remoteScript, bool forceRelease, HtmlHelper helper) {
+      return Asset(ScriptTagFormat, localScript, remoteScript, forceRelease, helper);
+    }
+
+    private static MvcHtmlString CssAsset(string localCss, string remoteCss, bool forceRelease, HtmlHelper helper) {
+      return Asset(CssTagFormat, localCss, remoteCss, forceRelease, helper);
     }
 
     public static MvcHtmlString JQuery(this HtmlHelper helper, bool forceRelease = false) {
@@ -31,6 +41,10 @@ namespace ClubPool.Web.Infrastructure
 
     public static MvcHtmlString JQueryUIScript(this HtmlHelper helper, bool forceRelease = false) {
       return ScriptAsset("jquery-ui.min.js", JQueryUIScriptPath, forceRelease, helper);
+    }
+
+    public static MvcHtmlString JQueryUICss(this HtmlHelper helper, bool forceRelease = false) {
+      return CssAsset("jquery-ui.css", JQueryUICssPath, forceRelease, helper);
     }
   }
 }
