eb294ad179011f5266343a94d8c094bb19041644
Paul Wideman <paul.wideman@gmail.com>
2012-02-11 18:46:34 -0600

    Added squishit javascriptbundle AddViewScripts extension, to search the views directory for all view scripts and add them automatically

--- src/ClubPool.Web/Infrastructure/ScriptExtensions.cs
+++ src/ClubPool.Web/Infrastructure/ScriptExtensions.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+using SquishIt.Framework.JavaScript;
 
 namespace ClubPool.Web.Infrastructure
 {
@@ -28,6 +32,48 @@ namespace ClubPool.Web.Infrastructure
       }
       else {
         return MvcHtmlString.Empty;
+      }
+    }
+
+    private static IEnumerable<string> viewScripts;
+    private static bool viewsDirectoryScanned = false;
+    private static object syncLock = new object();
+
+    public static JavaScriptBundle AddViewScripts(this JavaScriptBundle bundle, bool forceRelease = false) {
+      if (!forceRelease && HttpContext.Current.IsDebuggingEnabled) {
+        lock (syncLock) {
+          viewScripts = ScanViewsDirectory();
+          viewsDirectoryScanned = true;
+        }
+      }
+      else if (!viewsDirectoryScanned) {
+        lock (syncLock) {
+          if (!viewsDirectoryScanned) {
+            viewScripts = ScanViewsDirectory();
+            viewsDirectoryScanned = true;
+          }
+        }
+      }
+      bundle.Add(viewScripts.ToArray());
+      return bundle;
+    }
+
+    private static IEnumerable<string> ScanViewsDirectory() {
+      var appPath = DependencyResolver.Current.GetService<Configuration.ClubPoolConfiguration>().AppRootPath;
+      var scriptFiles = new List<string>();
+      var pathToScan = Path.Combine(appPath, "Views");
+      Trace.TraceInformation("Scanning path for view scripts: " + pathToScan);
+      ScanDirectoryForScripts(pathToScan, scriptFiles);
+      return scriptFiles.Select(s => s.Replace(appPath, "~/"));
+    }
+
+    private static void ScanDirectoryForScripts(string directory, List<string> scriptFiles) {
+      var myScriptFiles = Directory.GetFiles(directory, "scripts.js");
+      foreach (var file in myScriptFiles) {
+        scriptFiles.Add(file);
+      }
+      foreach (var subdirectory in Directory.GetDirectories(directory)) {
+        ScanDirectoryForScripts(subdirectory, scriptFiles);
       }
     }
   }
