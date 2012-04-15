9d2fb3b0213c32d7817f70d7398b42908403a2fd
Paul Wideman <paul.wideman@gmail.com>
2012-04-15 16:52:39 -0500

    Added image status icons to the schedule view

--- src/ClubPool.Web/Controllers/Shared/ViewModels/ScheduleViewModel.cs
+++ src/ClubPool.Web/Controllers/Shared/ViewModels/ScheduleViewModel.cs
@@ -19,7 +19,7 @@ namespace ClubPool.Web.Controllers.Shared.ViewModels
           Week = g.Key + 1,
           Date = startingDate.AddDays(g.Key * 7),
           Meets = g.Select(meet => new MeetViewModel(meet) {
-            Highlight = (null == teamToHighlight) ? false : meet.Teams.Contains(teamToHighlight)
+            Highlight = (null == teamToHighlight) ? false : meet.Teams.Contains(teamToHighlight),
           })
         });
       NumberOfMeetsPerWeek = Weeks.First().Meets.Count();
@@ -40,10 +40,10 @@ namespace ClubPool.Web.Controllers.Shared.ViewModels
   {
     public string Team1Name { get; set; }
     public string Team2Name { get; set; }
-    public bool IsComplete { get; set; }
     public int Week { get; set; }
     public int Id { get; set; }
     public bool Highlight { get; set; }
+    public MeetStatus Status { get; set; }
 
     public MeetViewModel() {
     }
@@ -51,14 +51,29 @@ namespace ClubPool.Web.Controllers.Shared.ViewModels
     public MeetViewModel(Meet meet) {
       Id = meet.Id;
       Week = meet.Week;
-      IsComplete = meet.IsComplete;
       var teams = meet.Teams.ToArray();
       var team1 = teams[0];
       var team2 = teams[1];
       Team1Name = team1.Name;
       Team2Name = team2.Name;
+      if (meet.IsComplete) {
+        Status = MeetStatus.Complete;
+      }
+      else {
+        if (meet.Matches.Any(m => m.IsComplete)) {
+          Status = MeetStatus.Partial;
+        }
+        else {
+          Status = MeetStatus.NotStarted;
+        }
+      }
     }
   }
 
-
+  public enum MeetStatus
+  {
+    NotStarted,
+    Partial,
+    Complete
+  }
 }
