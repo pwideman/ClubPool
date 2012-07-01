a8506bdbd0264b7cf6b0d1d49a54422e82365da5
Paul Wideman <paul.wideman@gmail.com>
2012-07-01 10:48:19 -0500

    Made Starting date input on edit division partial a date picker, this involved adding global metadata pattern and making the jqueryui datepicker trigger always have margin-left:6px

---
+++ src/ClubPool.Web/Views/Divisions/scripts.js
@@ -0,0 +1,13 @@
+﻿(function (sr) {
+
+  sr.registerViewScript("divisions/_editdivision", function (model) {
+    // set up date & time controls
+
+    $("input.divisions-datepicker").datepicker({
+      showOn: 'button',
+      buttonImage: model.calendarImageUrl,
+      buttonImageOnly: true
+    }).width(function (index, width) { return width - 26; });
+  });
+
+})($.scriptRegistrar);
